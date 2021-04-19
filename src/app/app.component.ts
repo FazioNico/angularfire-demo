b9ffad26d1253d677530d52198b940ec067bd538
FazioNico <contact@nicolasfazio.ch>
2021-04-19 10:36:51 +0200

    upd: explore collection query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FireDemo';
+  items$: Observable<any[]>;
+
+  constructor(
+    private _fireStore: AngularFirestore
+  ) {
+    this.items$ = this._fireStore.collection<any>(
+      'pipo', // nomdelacollection
+      // ref => ref.where('xxxx', '==', 'yyyyy') // systeme de query
+    )
+    .valueChanges({idField: 'key'}) // récupération 
+    .pipe(
+      tap(data => console.log(data))
+    );
+  }
+
+  check(item) {
+    console.log(item);
+  }
+  
+  async add() {
+    await this._fireStore.collection<any>('pipo').add({
+      xxxx: 'hello'
+    });
+    console.log('data save inside firebase');
+  }
 }
