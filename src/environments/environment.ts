d056e67aa54cb6d3853204f36aff9d5b33a0b5bc
FazioNico <contact@nicolasfazio.ch>
2021-04-19 09:47:34 +0200

    upd: add fb config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB_VAhJrs7Zj-lgSMFqgscbwqCL29GhAUg",
+    authDomain: "fir-21-536f3.firebaseapp.com",
+    databaseURL: "https://fir-21-536f3-default-rtdb.firebaseio.com",
+    projectId: "fir-21-536f3",
+    storageBucket: "fir-21-536f3.appspot.com",
+    messagingSenderId: "603943955028",
+    appId: "1:603943955028:web:ea67e682820001d40a5bc1"
+  }
 };
 
 /*
